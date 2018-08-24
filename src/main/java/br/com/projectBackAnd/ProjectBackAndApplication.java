9e1fc836ac866a10d80c7db3d77e9ab4acd25742
Guilherme <guil_stay@hotmail.com>
2018-08-24 16:11:32 -0300

    First Commit

---
+++ src/main/java/br/com/projectBackAnd/ProjectBackAndApplication.java
@@ -0,0 +1,12 @@
+package br.com.projectBackAnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectBackAndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectBackAndApplication.class, args);
+	}
+}
