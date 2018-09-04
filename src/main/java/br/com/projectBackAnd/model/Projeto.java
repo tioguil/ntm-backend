d69eb19da4ab1bf717fd2eb0afb63ff8900e4b89
Guilherme <guil_stay@hotmail.com>
2018-09-04 20:02:50 -0300

    Rout persist project

--- src/main/java/br/com/projectBackAnd/model/Projeto.java
+++ src/main/java/br/com/projectBackAnd/model/Projeto.java
@@ -2,7 +2,10 @@ package br.com.projectBackAnd.model;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
@@ -13,11 +16,31 @@ public class Projeto {
     private Long id;
     private String nome;
     private String descricao;
+    @DateTimeFormat(pattern="yyyy-MM-dd")
     private Date inicio;
+    @DateTimeFormat(pattern="yyyy-MM-dd")
     private Date fim;
     private Integer status;
     private Cliente cliente;
     private Usuario usuario;
     private List<Demanda> demanda;
+
+
+
+    /**
+     Recebe formato e retorna data no formato String já formadata
+     */
+    public String getInicio(String formato) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat(formato);
+        return dateFormat.format(this.inicio);
+    }
+
+    /**
+     Recebe formato e retorna data no formato String já formadata
+     */
+    public String getFim(String formato) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat(formato);
+        return dateFormat.format(this.fim);
+    }
 
 }
