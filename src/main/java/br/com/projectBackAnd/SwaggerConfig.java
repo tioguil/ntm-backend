80805e45715bc018b0eb1e736dc8805819c555c1
Guilherme <guil_stay@hotmail.com>
2018-09-16 14:39:11 -0300

    descricao de auth na api

--- src/main/java/br/com/projectBackAnd/SwaggerConfig.java
+++ src/main/java/br/com/projectBackAnd/SwaggerConfig.java
@@ -1,22 +1,20 @@
 package br.com.projectBackAnd;
 
-import org.springframework.context.annotation.Configuration;
-
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import org.springframework.context.annotation.Bean;
-
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.service.VendorExtension;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-
-
-import static springfox.documentation.builders.PathSelectors.regex;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 @Configuration
 @EnableSwagger2
@@ -31,6 +29,11 @@ public class SwaggerConfig {
                 .apis(RequestHandlerSelectors.basePackage("br.com.projectBackAnd"))
                 .paths(PathSelectors.any())
                 .build()
+                .globalOperationParameters(Collections.singletonList(new ParameterBuilder().name("Authorization")
+                .description("Bearer token")
+                .modelRef(new ModelRef("String"))
+                .parameterType("header")
+                .required(true).build()))
                 .apiInfo(metaInfo());
     }
 
