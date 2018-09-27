ae37628b34115b11ec3164a51cf432069276afa1
Guilherme <guil_stay@hotmail.com>
2018-09-27 20:36:19 -0300

    sql

--- src/main/java/br/com/projectBackEnd/Utili/PasswordEncoder.java
+++ src/main/java/br/com/projectBackEnd/Utili/PasswordEncoder.java
@@ -4,10 +4,10 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 public class PasswordEncoder {
     public static void main(String[] args) {
-        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder(10);
         String senhaEncod = bCryptPasswordEncoder.encode("123456");
         System.out.println(senhaEncod);
-        if(bCryptPasswordEncoder.matches("123455",senhaEncod )){
+        if(bCryptPasswordEncoder.matches("123456",senhaEncod )){
             System.out.println("Senha ok");
         }else {
             System.out.println("Senha nao conferem");
