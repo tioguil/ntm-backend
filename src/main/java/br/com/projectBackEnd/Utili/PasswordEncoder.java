6e7f008e02f86e6f9ffc029c426280cbbc5f4ab0
Guilherme Dos Santos <guil_stay@hotmail.com>
2018-10-06 13:50:44 -0300

    'commit'

--- src/main/java/br/com/projectBackEnd/Utili/PasswordEncoder.java
+++ src/main/java/br/com/projectBackEnd/Utili/PasswordEncoder.java
@@ -7,7 +7,7 @@ public class PasswordEncoder {
         BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder(10);
         String senhaEncod = bCryptPasswordEncoder.encode("123456");
         System.out.println(senhaEncod);
-        if(bCryptPasswordEncoder.matches("123456",senhaEncod )){
+        if(bCryptPasswordEncoder.matches("6a32cd50","$2a$10$vkSt7W4n4JyQQRgpgWi03uFtgSC3gKRbDDyFapcP911N3Le3T5NOS" )){
             System.out.println("Senha ok");
         }else {
             System.out.println("Senha nao conferem");
